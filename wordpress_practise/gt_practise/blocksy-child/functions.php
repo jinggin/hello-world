d40fe2f98206a1b513d4ea700e2e727b7bf9cdd2
yukijp <79243031+jinggin@users.noreply.github.com>
2021-05-26 21:19:24 +0900

    Blocksy theme trials

---
+++ wordpress_practise/gt_practise/blocksy-child/functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Blocksy Child Theme functions 
+ */
+if (! defined('WP_DEBUG')) {
+	die( 'Direct access forbidden.' );
+}
+
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_style( 'parent-style', 
+	get_template_directory_uri() . '/style.css' );
+});
+/**
+ * 
+ */
