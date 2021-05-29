f4551bdee3125feaf79eb66280455f5288ab2067
yukijp <79243031+jinggin@users.noreply.github.com>
2021-05-29 12:27:25 +0900

    Google analytics tag update

--- wordpress_practise/gt_practise/blocksy-child/header.php
+++ wordpress_practise/gt_practise/blocksy-child/header.php
@@ -22,6 +22,16 @@
 
 	<?php wp_head(); ?>
 	<?php do_action('blocksy:head:end') ?>
+		
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-LFRJ44GR4G"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-LFRJ44GR4G');
+	</script>
 </head>
 
 <body <?php body_class(); ?> <?php echo blocksy_body_attr() ?>>
